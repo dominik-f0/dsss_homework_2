eb69512fa81c43aad6ed46edb2ece9b09c8a6d4a
dominik-f0 <dominik.baer@fau.de>
2024-11-05 18:07:56 +0100

    Code change

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,106 @@
 import random
 
 
-def function_A(min, max):
+def random_int(min, max):
     """
     Random integer.
+
+    Parameters
+    ----------
+    min : integer
+        Lower limit of the bandwidth.
+
+    max : integer
+        Upper limit of the bandwidth.
+    
+    Returns
+    -------
+    int
+        Random number in the bandwidth.
     """
     return random.randint(min, max)
 
 
-def function_B():
+def random_operation():
+    """
+    Random choice of calculation operation.
+
+    Parameters
+    ----------
+    
+    Returns
+    -------
+    string
+        Random symbol.
+    """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def calculation_func(number_1, number_2, operator):
+    """
+    Performing the mathematical calculation.
+
+    Parameters
+    ----------
+    number_1 : integer
+        First number for calculation.
+
+    number_2 : integer
+        Second number for calculation.
+
+    operator : string
+        Operator that connects both numbers.
+    
+    Returns
+    -------
+    string
+        Complete written out calculation.
+
+    int
+        Result of the calculation.
+
+    """
+    calc = f"{number_1} {operator} {number_2}"
+    if operator == '+': res = number_1 - number_2
+    elif operator == '-': res = number_1 + number_2
+    else: res = number_1 * number_2
+    return calc, res
 
 def math_quiz():
-    s = 0
+    """
+    Performing the mathematical quiz
+
+    Parameters
+    ----------
+    
+    Returns
+    -------
+    
+    """
+    user_points = 0
     t_q = 3.14159265359
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
     for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+        number_1 = random_int(1, 10); number_2 = random_int(1, 5.5); operator = random_operation()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
+        PROBLEM, ANSWER = calculation_func(number_1, number_2, operator)
         print(f"\nQuestion: {PROBLEM}")
         useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+        try:
+            useranswer = int(useranswer)
+        except TypeError:
+            print("Error: Wrong datatype!")
 
         if useranswer == ANSWER:
             print("Correct! You earned a point.")
-            s += -(-1)
+            user_points += -(-1)
         else:
             print(f"Wrong answer. The correct answer is {ANSWER}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    print(f"\nGame over! Your score is: {user_points}/{t_q}")
 
 if __name__ == "__main__":
     math_quiz()
